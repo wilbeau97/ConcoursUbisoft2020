f2cf01e869603127180d80cbb5e9b3717b70c2fe
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-02-03 12:35:00 -0500

    second puzzle : door opening and minimal network physics

--- Assets/Script/Door.cs
+++ Assets/Script/Door.cs
@@ -6,10 +6,12 @@ public class Door : MonoBehaviour
 {
     [SerializeField] private Transform door;
     private bool open = false;
+
+    private Rigidbody _rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -23,7 +25,14 @@ public class Door : MonoBehaviour
     {
         StartCoroutine(OpenDoor());
     }
+
+    [PunRPC]
+    public void CloseDoorRPC()
+    {
+        StartCoroutine(CloseDoor());
+    }
     
+
     private IEnumerator OpenDoor()
     {
         while (door.position.y < 20)
@@ -31,5 +40,18 @@ public class Door : MonoBehaviour
             door.position += new Vector3(0, 1, 0);
             yield return null;
         }
+        _rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+    }
+    
+    private IEnumerator CloseDoor()
+    {
+        // while (door.position.y > 4)
+        // {
+        //     door.position -= new Vector3(0, 1, 0);
+        //     yield return null;
+        // }
+        _rigidbody.constraints = RigidbodyConstraints.None | RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+
+        yield return null;
     }
 }
