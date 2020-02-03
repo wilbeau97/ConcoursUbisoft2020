3d7cdb30d2a9a848619674e76d6928f45c4cb234
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-02-03 12:35:00 -0500

    second puzzle : door opening and minimal network physics

---
+++ Assets/Script/Plate3.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate3 : MonoBehaviour
+{
+    private bool isPressed;
+    [SerializeField] private PhotonView gameManagerView;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player2"))
+        {
+            isPressed = true;
+            Debug.Log("Plate3 IsPressed");
+            Pressed();
+        }
+    }
+    
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player2"))
+        {
+            isPressed = false;
+            Debug.Log("leave");
+            Pressed();
+        }
+    }
+
+    private void Pressed()
+    {
+        gameManagerView.RPC("Plate3", PhotonTargets.All, isPressed);
+    }
+    
+    
+    public virtual void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(isPressed);
+        } else if (stream.isReading)
+        {
+            isPressed = (bool) stream.ReceiveNext();
+        }
+    }
+}
