12115e85803eb77a6ff4847936463a6fa2683338
william <wil.beau@hotmail.com>
2020-02-04 11:11:48 -0500

    debut du UI et du passage de l'interface de depart, au jeu

---
+++ Assets/Script/UI/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+    public class MenuManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject startMenu;
+        [SerializeField] private GameObject playerChoiceMenu;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        public void Play()
+        {
+            startMenu.SetActive(false);
+            playerChoiceMenu.SetActive(true);
+        }
+    }
+
