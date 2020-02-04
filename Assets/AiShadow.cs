4c14e2405560505bde3e19936e6282a35d774281
unknown <vitrimel@gmail.com>
2020-02-04 18:32:04 -0500

    video

---
+++ Assets/AiShadow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiShadow : MonoBehaviour
+{
+    public GameObject shadow;
+    public int i = 10;
+    private Bounds area;
+    public int vitesse = 1;
+
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        area = new Bounds(gameObject.transform.position, new Vector3(i,i,i));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player1");
+        }
+        if (area.Contains(target.transform.position))
+        {
+            Debug.DrawRay(gameObject.transform.position,gameObject.transform.position - target.transform.position, Color.black);
+        }
+    }
+}
---
+++ Assets/testswitchscene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class testswitchscene : MonoBehaviour
+{
+    public int tick = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadSceneAsync("video", LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tick++;
+        if (tick == 60*10)
+        {
+            SceneManager.UnloadSceneAsync("video", UnloadSceneOptions.None);
+        }
+    }
+}
