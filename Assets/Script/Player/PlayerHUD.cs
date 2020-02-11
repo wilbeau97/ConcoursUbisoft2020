5e23a6fc45dc6b2a668e5d8b1fbc474697a684ff
william <wil.beau@hotmail.com>
2020-02-10 20:17:11 -0500

    try to fix UI

--- Assets/Script/Player/PlayerHUD.cs
+++ Assets/Script/Player/PlayerHUD.cs
@@ -13,7 +13,7 @@ public class PlayerHUD : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InteractableHintText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
