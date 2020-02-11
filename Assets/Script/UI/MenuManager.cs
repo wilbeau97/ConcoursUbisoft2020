ade20763538893d29a2ed318705758540541976e
william <wil.beau@hotmail.com>
2020-02-11 09:18:04 -0500

    controller ui avec manette

--- Assets/Script/UI/MenuManager.cs
+++ Assets/Script/UI/MenuManager.cs
@@ -1,19 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
-    public class MenuManager : MonoBehaviour
+public class MenuManager : MonoBehaviour
     {
         [SerializeField] private GameObject startMenu;
         [SerializeField] private GameObject playerChoiceMenu;
+
+        [SerializeField] private Button player1Button;
+        [SerializeField] private Button startButton;
+        [SerializeField] private Button player2Button;
         // Start is called before the first frame update
         void Start()
         {
-            
+            startButton.Select();
         }
 
         public void Play()
         {
             startMenu.SetActive(false);
             playerChoiceMenu.SetActive(true);
+            player1Button.Select();
+        }
+
+        public void ActivatedButtonPlayer1()
+        {
+            player1Button.Select();
+        }
+        
+        public void ActivatedButtonPlayer2()
+        {
+            player2Button.Select();
         }
     }
 
