16f5839f961fd120269fb6fff3709a261066ae16
william <wil.beau@hotmail.com>
2020-02-13 13:06:03 -0500

    debut du 1er puzzle du gym est bien commencer

---
+++ Assets/puzzle1Manager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzle1Manager : MonoBehaviour
+{
+    [SerializeField] private GameObject bridge;
+
+    [PunRPC]
+    public void RotateBridgeToPass()
+    {
+        StopCoroutine("RotateBridgeToBlockRoutine");
+        StartCoroutine("RotateBridgeToPassRoutine");
+    }
+
+    [PunRPC]
+    public void RotateBridgeToBlock()
+    {
+        StopCoroutine("RotateBridgeToPassRoutine");
+        StartCoroutine("RotateBridgeToBlockRoutine");
+    }
+
+    public IEnumerator RotateBridgeToPassRoutine()
+    {
+        Quaternion from = bridge.transform.rotation;
+        Quaternion to = bridge.transform.rotation;
+        to *= Quaternion.Euler( Vector3.up * 90 );
+    
+        float elapsed = 0.0f;
+        while( elapsed < 4f )
+        {
+            bridge.transform.rotation = Quaternion.Slerp(from, to, elapsed / 4f );
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        bridge.transform.rotation = to;
+    }
+    
+    public IEnumerator RotateBridgeToBlockRoutine()
+    {
+        Quaternion from = bridge.transform.rotation;
+        Quaternion to = bridge.transform.rotation;
+        to *= Quaternion.Euler( Vector3.up * -90 );
+    
+        float elapsed = 0.0f;
+        while( elapsed < 4f )
+        {
+            bridge.transform.rotation = Quaternion.Slerp(from, to, elapsed / 4f );
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        bridge.transform.rotation = to;
+    }
+    
+   
+}
