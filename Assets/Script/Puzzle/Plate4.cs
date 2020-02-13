9b5ab51b92db26b16edb45ec6f22fdcb8f60ab7e
william <wil.beau@hotmail.com>
2020-02-13 13:06:03 -0500

    debut du 1er puzzle du gym est bien commencer

---
+++ Assets/Script/Puzzle/Plate4.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private PhotonView puzzle1ManagerView;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!puzzle1ManagerView.isMine) return;
+        if (other.CompareTag("Player2") || other.CompareTag("Player1") || other.CompareTag("InteractablePhysicsObject")) 
+        {
+            Debug.Log("IsPressed");
+            puzzle1ManagerView.RPC("RotateBridgeToPass", PhotonTargets.All);
+        }
+    }
+    
+    public void OnTriggerExit(Collider other)
+    {
+        if (!puzzle1ManagerView.isMine) return;
+        if (other.CompareTag("Player2") || other.CompareTag("Player1") || other.CompareTag("InteractablePhysicsObject")) 
+        {
+            Debug.Log("leave");
+            puzzle1ManagerView.RPC("RotateBridgeToBlock", PhotonTargets.All);
+        }
+    }
+}
