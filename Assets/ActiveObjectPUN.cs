9d043bc99b8be888ee21062793f6bf8dd8354020
unknown <vitrimel@gmail.com>
2020-02-18 15:08:49 -0500

    mazeInclude

---
+++ Assets/ActiveObjectPUN.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveObjectPUN : MonoBehaviour
+{
+    [SerializeField] private GameObject targetGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    [PunRPC]
+    public void SetActive(Boolean value)
+    {
+        targetGameObject.SetActive(true);
+    }
+}
---
+++ Assets/PressurePlate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    private bool isPressed;
+    private PhotonView gameManagerView;
+    [SerializeField] private int playerTarget;
+    [SerializeField] private string punRPCMethodeName;
+    [SerializeField] private PressurePlate PairPressurePlate;
+
+    public void Start()
+    {
+        gameManagerView = gameObject.GetPhotonView();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"+playerTarget))
+        {
+            isPressed = true;
+            Debug.Log("IsPressed");
+            if(PairPressurePlate.isPressed)Pressed();
+        }
+    }
+    
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"+playerTarget))
+        {
+            isPressed = false;
+            Debug.Log("leave");
+            if(PairPressurePlate.isPressed)Pressed();
+        }
+    }
+
+    private void Pressed()
+    {
+        gameManagerView.RPC(punRPCMethodeName, PhotonTargets.All, isPressed);
+    }
+    
+    public virtual void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(isPressed);
+        } else if (stream.isReading)
+        {
+            isPressed = (bool) stream.ReceiveNext();
+        }
+    }
+}
