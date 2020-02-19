1e86f879469318f5c6aacfc76fba993dc436208e
william <wil.beau@hotmail.com>
2020-02-19 14:21:24 -0500

    cam n'est plus bloquer par les murs

---
+++ Assets/HideObjects.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObjects : MonoBehaviour
+{
+    
+    [SerializeField] private Transform player;
+    private RaycastHit[] hits = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hits != null)
+        {
+            foreach (RaycastHit hit in hits)
+            {
+                Renderer r = hit.collider.GetComponent<Renderer>();
+                if (r)
+                {
+                    r.enabled = true;
+                }
+            }
+        }
+
+        Debug.DrawRay(this.transform.position, (player.position - this.transform.position),
+            Color.magenta);
+
+        hits = Physics.RaycastAll(this.transform.position,
+            (player.position - this.transform.position),
+            Vector3.Distance(this.transform.position, player.position));
+
+        foreach (RaycastHit hit in hits)
+        {
+            Renderer r = hit.collider.GetComponent<Renderer>();
+            if (r)
+            {
+                r.enabled = false;
+            }
+        }
+    }
+}
