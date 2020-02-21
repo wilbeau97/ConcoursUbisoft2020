cc0373cf2c271ffcb8210dd7b8416ea6019ed7db
william <wil.beau@hotmail.com>
2020-02-20 20:58:31 -0500

    fix tuto qui etait pas vraiment parfait

--- Assets/Script/TutorialManager.cs
+++ Assets/Script/TutorialManager.cs
@@ -6,56 +6,15 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
-    private bool isStarted = false;
-    private bool isUsedAbility = false;
-    private bool isPuzzleEnded = false;
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!isStarted)
-        {
-            //press start to continue (enlever le texte de debut)
-            StartCoroutine("TutoTexte");
-            isStarted = true;
-        }
-    }
-
-    private IEnumerator TutoTexte()
-    {
-        while (!Input.GetButtonDown("Start"))
-        {
-            yield return null;
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player1") || other.CompareTag("Player2"))
         {
-            if (other.gameObject.GetPhotonView().isMine)
-            {
-                other.gameObject.GetComponent<Ability>().ActivateTutorial();
-            }
+            //if (other.gameObject.GetPhotonView().isMine)
+            //{
+                other.gameObject.GetComponent<Tutorial>().ActivateTutorial();
+           // }
         }
     }
-    
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player1") || other.CompareTag("Player2"))
-        {
-            if (other.gameObject.GetPhotonView().isMine)
-            {
-                other.gameObject.GetComponent<Ability>().DesactivateTutorial();
-                enabled = false;
-            }
-        }
-    }
-}
-
-public enum TutorialEnum
-{
-    START,
-    ABILITY,
-    ENDED_PUZZLE
 }
