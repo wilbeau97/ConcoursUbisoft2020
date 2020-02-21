8d7d0ae558646a9af30d9294cbb16d2884540e2e
unknown <vitrimel@gmail.com>
2020-02-21 12:34:17 -0500

    animator

---
+++ Assets/Script/AnimationControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void StopAnimation(string triggerName)
+    {
+        animator.SetTrigger(triggerName);
+    }
+
+    public void TriggerAnimation(string triggerName)
+    {
+        animator.SetTrigger(triggerName);
+    }
+}
