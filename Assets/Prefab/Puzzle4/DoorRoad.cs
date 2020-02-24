a6a6c4f00af158a0e0873c9c1186805a261f1a11
vitrimel@gmail.com <vitrimel@gmail.com>
2020-02-23 23:11:34 -0500

    pussle4

---
+++ Assets/Prefab/Puzzle4/DoorRoad.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Prefab.Puzzle4
+{
+    public class DoorRoad : MonoBehaviour
+    {
+        [SerializeField] private bool state;
+    
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 position = gameObject.transform.localPosition;
+            if (state && position.y < 10.0f)
+            {
+                gameObject.transform.Translate(0.0f,0.1f,0.0f);
+            }
+            else if (!state && position.y > 5.0f)
+            {
+                gameObject.transform.Translate(0.0f,-0.1f,0.0f);
+            }
+        }
+    
+        [PunRPC]
+        public void StateChange()
+        {
+            state = !state;
+        }
+    }
+}
