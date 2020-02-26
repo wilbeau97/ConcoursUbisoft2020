bb0bd8d5208fb8d2115a1940db9ab2b4b61ec016
vitrimel@gmail.com <vitrimel@gmail.com>
2020-02-26 14:55:28 -0500

    eazyPuzzle

---
+++ Assets/Script/AnimationGrabItem/DiamondPath.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Script.AnimationGrabItem
+{
+    public class DiamondPath : MonoBehaviour
+    {
+        [SerializeField] private Vector3[] diamondMarker;
+        private int _nextMaker = 0;
+
+        // Start is called before the first frame update
+        public void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.tag.Equals("Player1"))
+            {
+                if (_nextMaker < diamondMarker.Length)
+                {
+                    Transform parent = gameObject.transform.parent;
+                    gameObject.GetPhotonView().RPC("MoveDiamondPun", PhotonTargets.All, diamondMarker[_nextMaker]);
+                    _nextMaker++;
+                }
+                else
+                {
+                    Destroy(gameObject.transform.parent.gameObject);
+                }
+            }
+        }
+
+        [PunRPC]
+        public void MoveDiamondPun(Vector3 move)
+        {
+            StartCoroutine(MoveDiamond(move));
+        } 
+
+        public IEnumerator MoveDiamond(Vector3 move)
+        {
+            gameObject.transform.parent.position = move;
+            yield return null;
+        }
+    }
+}
