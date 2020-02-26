27018e270cc94d0b9eae7820815d2a1b9259937d
unknown <vitrimel@gmail.com>
2020-02-25 22:14:06 -0500

    areaObjective

---
+++ Assets/Prefab/MazeEnterArea/PuzzleEnterArea.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Prefab.MazeEnterArea
+{
+    public class PuzzleEnterArea : MonoBehaviour
+    {
+        private Text textArea;
+        public string objectif;
+
+        public void Start()
+        {
+            GameObject player1 = GameObject.FindGameObjectWithTag("Player1");
+            GameObject player2 = GameObject.FindGameObjectWithTag("Player2");
+            if (player1 != null)
+            {
+                Text[] texts = player1.GetComponentsInChildren<Text>();
+                foreach (Text text in texts)
+                {
+                    if (text.name.Equals("ObjectiveText"))
+                    {
+                        textArea = text;
+                    }
+                }
+            }
+            else if (player2 != null)
+            {
+                Text[] texts = player2.GetComponentsInChildren<Text>();
+                foreach (Text text in texts)
+                {
+                    if (text.name.Equals("ObjectiveText"))
+                    {
+                        textArea = text;
+                    }
+                }
+            }
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            textArea.text = objectif;
+        }
+    }
+}
