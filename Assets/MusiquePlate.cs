85e3c8391a1757e092ab3a8f3e5ccc4c5ae88ef0
vitrimel@gmail.com <vitrimel@gmail.com>
2020-02-26 19:24:33 -0500

    newscene

---
+++ Assets/MusiquePlate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Script.AnimationGrabItem;
+using UnityEngine;
+
+public class MusiquePlate : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other1)
+    {
+        AudioManager.Instance.Play("Chest Opening Sound Legend of Zelda Ocarina of Time");
+    }
+}
