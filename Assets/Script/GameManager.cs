f575f8bbbf1e38f4581eebcc2878e6a5b68b26c0
william <wil.beau@hotmail.com>
2020-02-29 22:42:26 -0500

    ouverture des portes done

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,23 +6,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private bool plate1 = false;
-    private bool plate2 = false;
-    [SerializeField] private bool plate3 = false;
-    private bool open = false;
-    [SerializeField] private bool open2 = false;
-    [SerializeField] private PhotonView doorView;
     [SerializeField] private GameObject playerPrefab;
-    [SerializeField] private PhotonView doorView2;
-    [SerializeField] private PhotonView cubeView;
     [SerializeField] private Transform spawnPointP1;
     [SerializeField] private Transform spawnPointP2;
     [SerializeField] private Transform spawnPointNotconnected;
 
     [SerializeField] private BigTree tree;
-    //private GameObject player1;
-    //private GameObject player2;
-
+    [SerializeField] private PhotonView[] doorViews;
+    private int nbOfPuzzleSuceeed = 0;
     private void Awake()
     {
         if (PhotonNetwork.connected)
@@ -55,62 +46,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (plate1 == true && plate2 == true && open == false)
-        {
-            doorView.RPC("OpenDoorRPC", PhotonTargets.All);
-            open = true;
-        }
-
-        if (plate3 == true && open2 == false)
-        {
-            doorView2.RPC("OpenDoorRPC", PhotonTargets.All);
-            open2 = true;
-        }
-
-        if (plate3 == false && open2 == true)
-        {
-            doorView2.RPC("CloseDoorRPC", PhotonTargets.All);
-            open2 = false;
-        }
-    }
-
-    [PunRPC] 
-    public void Plate1(bool v)
-    {
-        plate1 = v;
+        
     }
     
-    [PunRPC] 
-    public void Plate2(bool v)
-    {
-        plate2 = v;
-    }
-    
-    [PunRPC] 
-    public void Plate3(bool v)
-    {
-        plate3 = v;
-    }
-
-    public virtual void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.isWriting)
-        {
-            stream.SendNext(plate1);
-            stream.SendNext(plate2);
-            stream.SendNext(plate3);
-        } else if (stream.isReading)
-        {
-            plate1 = (bool) stream.ReceiveNext();
-            plate2 = (bool) stream.ReceiveNext();
-            plate3 = (bool) stream.ReceiveNext();
-        }
-    }
-
     [PunRPC]
     public void EndedPuzzle()
     {
         tree.Grow();
         PlayerManager.LocalPlayerInstance.GetComponent<PlayerNetwork>().EndedPuzzle();
+        OpenNextDoor();
+        nbOfPuzzleSuceeed += 1;
+    }
+
+    private void OpenNextDoor()
+    {
+        doorViews[nbOfPuzzleSuceeed].RPC("OpenDoorRPC", PhotonTargets.All);
     }
 }
