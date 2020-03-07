b1a9ea7c83d3e799cba31038c1c41284cb2b32c0
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-03-07 14:10:17 -0500

    intro puzzle 1 et modif/ajout script de portes

--- Assets/Script/Puzzle/Door.cs
+++ Assets/Script/Puzzle/Door.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField] private Transform door;
+    [SerializeField] private float maxOpeningHeight = 20;
+    [SerializeField] private bool releaseRigidBody = true;
+    [SerializeField] private float openingSpeed = 1;
     private bool open = false;
+    private float initialPositionY;
+    
 
     private Rigidbody _rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        if (releaseRigidBody)
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        initialPositionY = door.position.y;
+
     }
 
     // Update is called once per frame
@@ -36,20 +47,28 @@ public class Door : MonoBehaviour
     public IEnumerator OpenDoor()
     {
         StopCoroutine(CloseDoor());
-        while (door.position.y < 20)
+        while (door.position.y <= maxOpeningHeight+ initialPositionY)
         {
-            door.position += new Vector3(0, 1, 0);
+            door.position += new Vector3(0, openingSpeed, 0);
             yield return null;
         }
         
-        _rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        if(releaseRigidBody){_rigidbody.constraints = RigidbodyConstraints.FreezeAll;}
     }
     
     public IEnumerator CloseDoor()
     {
         StopCoroutine(OpenDoor());
-        _rigidbody.constraints = RigidbodyConstraints.None | RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
-
+        if (releaseRigidBody) {
+            _rigidbody.constraints = RigidbodyConstraints.None | RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+        } 
+        else {
+            while (door.position.y > initialPositionY)
+            {
+                door.position -= new Vector3(0, openingSpeed, 0);
+                yield return null;
+            }
+        }
         yield return null;
     }
     
