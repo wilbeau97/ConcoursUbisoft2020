00dbc0d908e072582469ac5a3f162a4f5361df00
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-03-08 09:02:53 -0400

    commit pre-merge master

---
+++ Assets/Script/Player/endPuzzleProgression.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endPuzzleProgression : Photon.MonoBehaviour
+{
+    [SerializeField] private string playerTagToLevelUp;
+    private PhotonView gm;
+    private bool alreadyTrigger = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetPhotonView();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTagToLevelUp))
+        {
+            other.GetComponent<TutorialP1>().activateDoubleJumpText();
+        }
+    }
+    
+    public virtual void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(alreadyTrigger);
+        } else if (stream.isReading)
+        {
+            alreadyTrigger = (bool) stream.ReceiveNext();
+        }
+    }
+}
