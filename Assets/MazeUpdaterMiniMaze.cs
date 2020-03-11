123f2a273bc004cd829d937387135f210bf6f4a0
unknown <vitrimel@gmail.com>
2020-03-11 11:21:49 -0400

    maze

---
+++ Assets/MazeUpdaterMiniMaze.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeUpdaterMiniMaze : MonoBehaviour
+{
+    private MiniMaze[] miniMazes;
+    public GameObject redCube;
+    public GameObject blueCube;
+    public GameObject greenCube;
+    private bool red = false;
+    private bool blue = false;
+    private bool green = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        miniMazes = GetComponentsInChildren<MiniMaze>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (redCube.activeSelf)
+        {
+            foreach (MiniMaze miniMaze in miniMazes)
+            {
+                miniMaze.redCube.SetActive(true);
+            }
+        }
+
+        if (blueCube.activeSelf)
+        {
+            foreach (MiniMaze miniMaze in miniMazes)
+            {
+                miniMaze.blueCube.SetActive(true);
+            }
+        }
+
+        if (greenCube.activeSelf)
+        {
+            foreach (MiniMaze miniMaze in miniMazes)
+            {
+                miniMaze.greenCube.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/MiniMaze.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMaze : MonoBehaviour
+{
+    public GameObject redCube;
+    public GameObject blueCube;
+    public GameObject greenCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
