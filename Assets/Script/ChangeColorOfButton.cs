d62ae4f1c2efd4afc100bae9397a67c09ef30329
william <wil.beau@hotmail.com>
2020-03-21 10:38:02 -0400

    ajouter les concepts art, et modifier le UI de depart

---
+++ Assets/Script/ChangeColorOfButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChangeColorOfButton : MonoBehaviour, ISelectHandler,IDeselectHandler
+{
+    [SerializeField] private Image button;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        button.color = Color.cyan;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        button.color = Color.white;
+    }
+}
