3e3e6adf2d5aec5b347d2a93e7c45f60a73da6da
william <wil.beau@hotmail.com>
2020-03-23 21:24:14 -0400

    fog works

---
+++ Assets/Volume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AuraAPI;
+using UnityEngine;
+
+public class Volume : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AuraVolume>().gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
