5f200dcdcaed0b7a3a7fd961f0ff8bd72ce1302d
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-03-29 14:00:13 -0400

    jumphigh random fonctinne et en réseau aussi!!

--- Assets/Script/Puzzle/positionSelector.cs
+++ Assets/Script/Puzzle/positionSelector.cs
@@ -7,7 +7,7 @@ public class PositionSelector : MonoBehaviour
     {
         public PuzzleGenerator assignedPuzzleGenerator;
         private bool is1StPlaytrough = true;
-
+        
         public int selectedPosition = 0;
         // liste des positions possible de cet objet pour un premier playthrough
         private Transform gameObjectPosition;
@@ -16,9 +16,9 @@ public class PositionSelector : MonoBehaviour
         public Transform[] possiblePositions2NdPlaytrough;
         private string localPlayerName;
     
-        void Start()
+        void Awake()
         {
-            gameObjectPosition = this.GetComponent<Transform>();
+            gameObjectPosition = GetComponent<Transform>();
         }
 
         public void RandomSelectPosition()
@@ -27,6 +27,7 @@ public class PositionSelector : MonoBehaviour
             {
                 if (possiblePositions1StPlaytrough.Length != 0) // si aucune position, on garde la position actuelle
                 {
+                    Random.InitState(DateTime.Now.Millisecond); // pour s'assurer que ça plus random (et peut-être lorsqu'on aura un system de seed)
                     int index = Random.Range(0, possiblePositions1StPlaytrough.Length);
                     selectedPosition = index;
                     gameObjectPosition.transform.position = possiblePositions1StPlaytrough[index].transform.position;
@@ -35,6 +36,7 @@ public class PositionSelector : MonoBehaviour
             if (!is1StPlaytrough)
             {
                 if (possiblePositions2NdPlaytrough.Length == 0) return; // si aucune position, on garde la dernière position
+                Random.InitState(DateTime.Now.Millisecond); // pour s'assurer que ça plus random (et peut-être lorsqu'on aura un system de seed)
                 int index = Random.Range(0, possiblePositions2NdPlaytrough.Length);
                 selectedPosition = index;
                 gameObjectPosition.transform.position = possiblePositions2NdPlaytrough[index].transform.position;
@@ -49,6 +51,7 @@ public class PositionSelector : MonoBehaviour
                 if (possiblePositions1StPlaytrough.Length != 0 &&
                     (arrayPosition <= possiblePositions1StPlaytrough.Length))
                 {
+                    selectedPosition = arrayPosition;
                     gameObjectPosition.transform.position = possiblePositions1StPlaytrough[arrayPosition].transform.position;
                     return;
                 }
@@ -58,6 +61,7 @@ public class PositionSelector : MonoBehaviour
                 if (possiblePositions2NdPlaytrough.Length != 0 && 
                     (arrayPosition <= possiblePositions2NdPlaytrough.Length))
                 {
+                    selectedPosition = arrayPosition;
                     gameObjectPosition.transform.position = possiblePositions2NdPlaytrough[arrayPosition].transform.position;
                 }
             }
@@ -83,16 +87,24 @@ public class PositionSelector : MonoBehaviour
             localPlayerName = pLocalPlayerName;
         }
         
-        // public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-        // {
-        //     if (stream.isWriting)
-        //     {
-        //         stream.SendNext(matIsOn);
-        //     } else if (stream.isReading)
-        //     {
-        //         matIsOn = (bool) stream.ReceiveNext();
-        //     }
-        // }
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (localPlayerName == "Player 1(Clone)")
+            {
+                if (stream.isWriting)
+                {
+                    stream.SendNext(selectedPosition);
+                } else if (stream.isReading)
+                {
+                    selectedPosition = (int) stream.ReceiveNext();
+                }
+            }
+            else if (stream.isReading)
+            {
+                selectedPosition = (int) stream.ReceiveNext();
+            }
+            
+        }
         
     }
 
