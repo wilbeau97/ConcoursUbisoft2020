f83fe0a9edb1a4a93823454351a546cab2ccb02f
AlexandreMoreau96 <48135508+AlexandreMoreau96@users.noreply.github.com>
2020-04-05 19:06:57 -0400

    Room Selection Complete

--- Assets/Script/UI/MenuManager.cs
+++ Assets/Script/UI/MenuManager.cs
@@ -4,11 +4,16 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
     {
         [SerializeField] private GameObject startMenu;
-        [SerializeField] private GameObject playerChoiceMenu;
-
+        [SerializeField] private GameObject roomMenu;
+        [SerializeField] private GameObject typeRoomToJoinPanel;
+        [SerializeField] private GameObject typeRoomToCreatePanel;
         [SerializeField] private Button player1Button;
         [SerializeField] private Button startButton;
         [SerializeField] private Button player2Button;
+        [SerializeField] private InputField roomNameCreate;
+        [SerializeField] private InputField roomNameJoin;
+        [SerializeField] private Launcher launcher;
+        
         // Start is called before the first frame update
         void Start()
         {
@@ -18,8 +23,63 @@ public class MenuManager : MonoBehaviour
         public void Play()
         {
             startMenu.SetActive(false);
-            playerChoiceMenu.SetActive(true);
+            roomMenu.SetActive(true);
             player1Button.Select();
+        }
+
+        public void CreateRoom()
+        {
+            RoomInfo[] rooms = PhotonNetwork.GetRoomList();
+
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                if (rooms[i].Name == roomNameCreate.text)
+                {
+                    Debug.Log("This room exists !");
+                    return;
+                }
+            } 
+            
+            launcher.roomName = roomNameCreate.text;
+            launcher.Connect();
+            roomMenu.SetActive(true);
+            startMenu.SetActive(false);
+        }
+
+        public void TypeRoomNameToJoin()
+        {
+            typeRoomToJoinPanel.SetActive(true);
+        }
+
+        public void TypeRoomNameToCreate()
+        {
+            typeRoomToCreatePanel.SetActive(true);
+        }
+        
+        public void CancelJoin()
+        {
+            typeRoomToJoinPanel.SetActive(false);   
+            typeRoomToCreatePanel.SetActive(false);   
+        }
+        
+        public void JoinRoom()
+        {
+            RoomInfo[] rooms = PhotonNetwork.GetRoomList();
+
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                if (rooms[i].Name == roomNameJoin.text)
+                {
+                    Debug.Log("This room exists !");
+                    launcher.roomName = roomNameJoin.text;
+                    launcher.Connect();
+                    startMenu.SetActive(false);
+                    roomMenu.SetActive(true);
+                    return;
+                }
+            }
+            
+            Debug.Log("Room doesn't exist!");
         }
 
         public void Exit()
