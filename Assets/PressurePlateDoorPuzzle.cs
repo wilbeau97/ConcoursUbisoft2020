2f1093aa589600561171d430be150802e605554c
william <wil.beau@hotmail.com>
2020-04-08 12:32:06 -0400

    debut du puzzle

---
+++ Assets/PressurePlateDoorPuzzle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateDoorPuzzle : MonoBehaviour
+{
+    [SerializeField] private PhotonView doorToOpen;
+    [SerializeField] private PhotonView partOfPuzzleManager;
+    // Start is cal led before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player1") || other.CompareTag("InteractablePhysicsObject"))
+        {
+            doorToOpen.RPC("OpenDoorRPC", PhotonTargets.All);
+            //doorToOpen.gameObject.GetComponent<Door>().OpenDoorRPC();
+            //partOfPuzzleManager.gameObject.GetComponent<PartOfDoorPuzzleManager>().DeactivateAllPressurePlate();
+            partOfPuzzleManager.RPC("DeactivateAllPressurePlate", PhotonTargets.All);
+        }
+    }
+}
---
+++ Assets/Script/Puzzle/PartOfDoorPuzzleManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartOfDoorPuzzleManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] pressurePlates;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [PunRPC]
+    public void DeactivateAllPressurePlate()
+    {
+        foreach (GameObject pressurePlate in pressurePlates)
+        {
+            pressurePlate.SetActive(false);
+        }
+    }
+}
