89f0a4b268f89a699bfb1d7d0b62217314e34aa0
william <wil.beau@hotmail.com>
2020-04-09 09:31:24 -0400

    puzzle completement fonctionnel, manque seulement le random de chemin

--- Assets/Script/Puzzle/PartOfDoorPuzzleManager.cs
+++ Assets/Script/Puzzle/PartOfDoorPuzzleManager.cs
@@ -5,17 +5,7 @@ using UnityEngine;
 public class PartOfDoorPuzzleManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] pressurePlates;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private Door[] doors;
 
     [PunRPC]
     public void DeactivateAllPressurePlate()
@@ -23,6 +13,29 @@ public class PartOfDoorPuzzleManager : MonoBehaviour
         foreach (GameObject pressurePlate in pressurePlates)
         {
             pressurePlate.SetActive(false);
+        }
+    }
+
+    public void Restart()
+    {
+        ActivateAllPressurePlate();
+        CloseAllDoor();
+    }
+    
+    private void ActivateAllPressurePlate()
+    {
+        foreach (GameObject pressurePlate in pressurePlates)
+        {
+            pressurePlate.SetActive(true);
+        }
+    }
+    
+    private void CloseAllDoor()
+    {
+        foreach (Door door in doors)
+        {
+            if(door.alreadyOpen)
+                door.CloseDoorRPC();
         }
     }
 }
--- Assets/Script/Puzzle1ManagerVariant.cs
+++ Assets/Script/Puzzle1ManagerVariant.cs
@@ -1,37 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ExitGames.Demos.DemoAnimator;
 using UnityEngine;
 
-public class Puzzle1ManagerVariant : MonoBehaviour
+public class Puzzle1ManagerVariant : MonoBehaviour, IPunObservable
 {
-    [SerializeField] private Transform respawnPoint;
-    private Transform playerToRespawn;
+    [SerializeField] private PartOfDoorPuzzleManager[] partOfDoorPuzzleManagers;
+    [SerializeField] private Transform respawnPointP1;
+    [SerializeField] private Transform respawnPointP2;
+
+    private GameObject localPlayer;
+    private bool isNotRespawn = true;
+    private PhotonView view;
+    private Transform respawnPoint;
+    
     // Start is called before the first frame update
     void Start()
     {
+        view = gameObject.GetPhotonView();
+        localPlayer = GameObject.FindWithTag(PlayerManager.LocalPlayerInstance.tag);
         
+        if (PlayerManager.LocalPlayerInstance.CompareTag("Player1"))
+        {
+            respawnPoint = respawnPointP1;
+            
+        } else if(PlayerManager.LocalPlayerInstance.CompareTag("Player2"))
+        {
+            respawnPoint = respawnPointP2;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (localPlayer == null )
+        {
+            localPlayer = GameObject.FindWithTag(PlayerManager.LocalPlayerInstance.tag);
+        }
+    }
+
+    [PunRPC]
+    private void Respawn()
+    {
+        localPlayer.GetComponentInChildren<PlayerHUD>().FadeOut();
+        StartCoroutine("WaitForAnimation");
     }
     
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player1") || other.CompareTag("Player2"))
+        if (isNotRespawn && (other.CompareTag("Player1") || other.CompareTag("Player2")))
         {
-            other.gameObject.GetComponentInChildren<PlayerHUD>().FadeOut();
-            playerToRespawn = other.transform;
-            StartCoroutine("WaitForAnimation");
+            isNotRespawn = false;
+            if (PhotonNetwork.connected)
+            {
+                view.RPC("Respawn", PhotonTargets.All);
+            }
+            else
+            {
+                Respawn();
+            }
         }
     }
 
     private IEnumerator WaitForAnimation()
     {
+        foreach (PartOfDoorPuzzleManager manager in partOfDoorPuzzleManagers)
+        {
+            manager.Restart();
+        }
         yield return new WaitForSeconds(1f);
-        playerToRespawn.position = respawnPoint.position;
-        playerToRespawn.gameObject.GetComponentInChildren<PlayerHUD>().FadeIn();
+        localPlayer.transform.position = respawnPoint.position;
+        localPlayer.GetComponentInChildren<PlayerHUD>().FadeIn();
+        isNotRespawn = true;
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(isNotRespawn);
+        } else if (stream.isReading)
+        {
+            isNotRespawn = (bool) stream.ReceiveNext();
+        }
     }
 }
